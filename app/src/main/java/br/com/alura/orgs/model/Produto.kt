b263d82625311d757ce3931a12bf4e2018d12787
Eduardo F Costa <e.feitosa@spotpromo.com.br>
2023-05-02 22:05:06 -0300

    c3a01/02 - implementando room

--- app/src/main/java/br/com/alura/orgs/model/Produto.kt
+++ app/src/main/java/br/com/alura/orgs/model/Produto.kt
@@ -1,10 +1,18 @@
 package br.com.alura.orgs.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.math.BigDecimal
 
+@Entity
 data class Produto(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val nome: String,
     val descricao: String,
     val valor: BigDecimal,
     val imagem: String? = null
-)
+) {
+    constructor(nome: String, descricao: String, valor: BigDecimal, imagem: String?) :
+            this(0, nome, descricao, valor, imagem)
+}
